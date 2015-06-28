6f1677a46825653c7fba073491544a3b8ffabdb1
Pierdebeer <pierdebeer@libero.it>
2015-06-28 15:39:37 +0200

    Patches ported from the MEA LL source

--- arch/arm/hotplug/.built-in.o.cmd
+++ arch/arm/hotplug/.built-in.o.cmd
@@ -1,1 +1,1 @@
-cmd_arch/arm/hotplug/built-in.o :=  /home/slim80/Scrivania/kernel/Compilatori/linaro_a15_4.9.3-2015.03/bin/arm-eabi-ld -EL    -r -o arch/arm/hotplug/built-in.o arch/arm/hotplug/intelli_plug.o 
+cmd_arch/arm/hotplug/built-in.o :=  /home/audax/linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin/arm-cortex_a15-linux-gnueabihf-ld -EL    -r -o arch/arm/hotplug/built-in.o arch/arm/hotplug/intelli_plug.o 
--- arch/arm/hotplug/.intelli_plug.o.cmd
+++ arch/arm/hotplug/.intelli_plug.o.cmd
@@ -1,4 +1,4 @@
-cmd_arch/arm/hotplug/intelli_plug.o := /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/scripts/gcc-wrapper.py ccache /home/slim80/Scrivania/kernel/Compilatori/linaro_a15_4.9.3-2015.03/bin/arm-eabi-gcc -Wp,-MD,arch/arm/hotplug/.intelli_plug.o.d  -nostdinc -isystem /home/slim80/Scrivania/kernel/Compilatori/linaro_a15_4.9.3-2015.03/bin/../lib/gcc/arm-cortex_a15-linux-gnueabihf/4.9.3/include -I/home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include -Iarch/arm/include/generated -Iinclude  -include /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-msm/include -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -Wno-sizeof-pointer-memaccess -Wno-aggressive-loop-optimizations -Wno-sequence-point -Wno-unused-variable -Wno-unused-function -fno-delete-null-pointer-checks -marm -mtune=cortex-a15 -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mvectorize-with-neon-quad -fno-tree-vectorize -fmodulo-sched -ffast-math -funsafe-math-optimizations -Os -Wno-maybe-uninitialized -marm -fno-dwarf2-cfi-asm -fstack-protector -mabi=aapcs-linux -mno-thumb-interwork -funwind-tables -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a15 -msoft-float -Uarm -Wframe-larger-than=1024 -Wno-unused-but-set-variable -fomit-frame-pointer -fno-var-tracking-assignments -g -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -DCC_HAVE_ASM_GOTO   -marm -mtune=cortex-a15 -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mvectorize-with-neon-quad -fno-tree-vectorize -fmodulo-sched -ffast-math -funsafe-math-optimizations -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(intelli_plug)"  -D"KBUILD_MODNAME=KBUILD_STR(intelli_plug)" -c -o arch/arm/hotplug/intelli_plug.o arch/arm/hotplug/intelli_plug.c
+cmd_arch/arm/hotplug/intelli_plug.o := /home/audax/Audax_Kernel/scripts/gcc-wrapper.py ccache /home/audax/linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin/arm-cortex_a15-linux-gnueabihf-gcc -Wp,-MD,arch/arm/hotplug/.intelli_plug.o.d  -nostdinc -isystem /home/audax/linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/bin/../lib/gcc/arm-cortex_a15-linux-gnueabihf/4.9.3/include -I/home/audax/Audax_Kernel/arch/arm/include -Iarch/arm/include/generated -Iinclude  -include /home/audax/Audax_Kernel/include/linux/kconfig.h -D__KERNEL__ -mlittle-endian -Iarch/arm/mach-msm/include -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -Wno-sizeof-pointer-memaccess -Wno-aggressive-loop-optimizations -Wno-sequence-point -Wno-unused-variable -Wno-unused-function -fno-delete-null-pointer-checks -marm -mtune=cortex-a15 -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mvectorize-with-neon-quad -fno-tree-vectorize -fmodulo-sched -ffast-math -funsafe-math-optimizations -Os -Wno-maybe-uninitialized -marm -fno-dwarf2-cfi-asm -fstack-protector -mabi=aapcs-linux -mno-thumb-interwork -funwind-tables -D__LINUX_ARM_ARCH__=7 -mcpu=cortex-a15 -msoft-float -Uarm -Wframe-larger-than=1024 -Wno-unused-but-set-variable -fomit-frame-pointer -fno-var-tracking-assignments -g -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fconserve-stack -DCC_HAVE_ASM_GOTO   -marm -mtune=cortex-a15 -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mvectorize-with-neon-quad -fno-tree-vectorize -fmodulo-sched -ffast-math -funsafe-math-optimizations -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(intelli_plug)"  -D"KBUILD_MODNAME=KBUILD_STR(intelli_plug)" -c -o arch/arm/hotplug/intelli_plug.o arch/arm/hotplug/intelli_plug.c
 
 source_arch/arm/hotplug/intelli_plug.o := arch/arm/hotplug/intelli_plug.c
 
@@ -31,7 +31,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/arch/dma/addr/t/64bit.h) \
     $(wildcard include/config/phys/addr/t/64bit.h) \
     $(wildcard include/config/64bit.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/types.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/types.h \
   include/asm-generic/int-ll64.h \
   arch/arm/include/generated/asm/bitsperlong.h \
   include/asm-generic/bitsperlong.h \
@@ -47,7 +47,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/arch/supports/optimized/inlining.h) \
     $(wildcard include/config/optimize/inlining.h) \
   include/linux/compiler-gcc4.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/posix_types.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/posix_types.h \
   include/asm-generic/posix_types.h \
   include/linux/poison.h \
     $(wildcard include/config/illegal/pointer/value.h) \
@@ -68,22 +68,22 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/compaction.h) \
     $(wildcard include/config/ftrace/mcount/record.h) \
   include/linux/sysinfo.h \
-  /home/slim80/Scrivania/kernel/Compilatori/linaro_a15_4.9.3-2015.03/lib/gcc/arm-cortex_a15-linux-gnueabihf/4.9.3/include/stdarg.h \
+  /home/audax/linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.03/lib/gcc/arm-cortex_a15-linux-gnueabihf/4.9.3/include/stdarg.h \
   include/linux/linkage.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/linkage.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/linkage.h \
   include/linux/bitops.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/bitops.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/bitops.h \
   include/linux/irqflags.h \
     $(wildcard include/config/trace/irqflags.h) \
     $(wildcard include/config/irqsoff/tracer.h) \
     $(wildcard include/config/preempt/tracer.h) \
     $(wildcard include/config/trace/irqflags/support.h) \
   include/linux/typecheck.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/irqflags.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/ptrace.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/irqflags.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/ptrace.h \
     $(wildcard include/config/cpu/endian/be8.h) \
     $(wildcard include/config/arm/thumb.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/hwcap.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/hwcap.h \
   include/asm-generic/bitops/non-atomic.h \
   include/asm-generic/bitops/fls64.h \
   include/asm-generic/bitops/sched.h \
@@ -92,10 +92,10 @@ deps_arch/arm/hotplug/intelli_plug.o := \
   include/asm-generic/bitops/const_hweight.h \
   include/asm-generic/bitops/lock.h \
   include/asm-generic/bitops/le.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/byteorder.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/byteorder.h \
   include/linux/byteorder/little_endian.h \
   include/linux/swab.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/swab.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/swab.h \
   include/linux/byteorder/generic.h \
   include/asm-generic/bitops/ext2-atomic-setbit.h \
   include/linux/log2.h \
@@ -110,9 +110,9 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/modules.h) \
     $(wildcard include/config/hotplug.h) \
   include/linux/dynamic_debug.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/div64.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/compiler.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/bug.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/div64.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/compiler.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/bug.h \
     $(wildcard include/config/bug.h) \
     $(wildcard include/config/thumb2/kernel.h) \
     $(wildcard include/config/debug/bugverbose.h) \
@@ -120,7 +120,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
   include/asm-generic/bug.h \
     $(wildcard include/config/generic/bug.h) \
     $(wildcard include/config/generic/bug/relative/pointers.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/cache.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/cache.h \
     $(wildcard include/config/arm/l1/cache/shift.h) \
     $(wildcard include/config/aeabi.h) \
   include/linux/seqlock.h \
@@ -135,40 +135,40 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/preempt/notifiers.h) \
   include/linux/thread_info.h \
     $(wildcard include/config/compat.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/thread_info.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/thread_info.h \
     $(wildcard include/config/arm/thumbee.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/fpstate.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/fpstate.h \
     $(wildcard include/config/vfpv3.h) \
     $(wildcard include/config/iwmmxt.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/domain.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/domain.h \
     $(wildcard include/config/verify/permission/fault.h) \
     $(wildcard include/config/io/36.h) \
     $(wildcard include/config/cpu/use/domains.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/barrier.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/barrier.h \
     $(wildcard include/config/cpu/32v6k.h) \
     $(wildcard include/config/cpu/xsc3.h) \
     $(wildcard include/config/cpu/fa526.h) \
     $(wildcard include/config/arch/has/barriers.h) \
     $(wildcard include/config/arm/dma/mem/bufferable.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/outercache.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/outercache.h \
     $(wildcard include/config/outer/cache/sync.h) \
     $(wildcard include/config/outer/cache.h) \
   include/linux/stringify.h \
   include/linux/bottom_half.h \
   include/linux/spinlock_types.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/spinlock_types.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/spinlock_types.h \
   include/linux/lockdep.h \
     $(wildcard include/config/lock/stat.h) \
     $(wildcard include/config/prove/rcu.h) \
   include/linux/rwlock_types.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/spinlock.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/spinlock.h \
     $(wildcard include/config/msm/krait/wfe/fixup.h) \
     $(wildcard include/config/arm/ticket/locks.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/processor.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/processor.h \
     $(wildcard include/config/have/hw/breakpoint.h) \
     $(wildcard include/config/mmu.h) \
     $(wildcard include/config/arm/errata/754327.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/hw_breakpoint.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/hw_breakpoint.h \
   include/linux/rwlock.h \
   include/linux/spinlock_api_smp.h \
     $(wildcard include/config/inline/spin/lock.h) \
@@ -203,8 +203,8 @@ deps_arch/arm/hotplug/intelli_plug.o := \
   include/linux/atomic.h \
     $(wildcard include/config/arch/has/atomic/or.h) \
     $(wildcard include/config/generic/atomic64.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/atomic.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/cmpxchg.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/atomic.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/cmpxchg.h \
     $(wildcard include/config/cpu/sa1100.h) \
     $(wildcard include/config/cpu/sa110.h) \
     $(wildcard include/config/cpu/v6.h) \
@@ -214,9 +214,9 @@ deps_arch/arm/hotplug/intelli_plug.o := \
   include/linux/jiffies.h \
   include/linux/timex.h \
   include/linux/param.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/param.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/param.h \
     $(wildcard include/config/hz.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/timex.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/timex.h \
   arch/arm/mach-msm/include/mach/timex.h \
     $(wildcard include/config/have/arch/has/current/timer.h) \
   include/linux/debugobjects.h \
@@ -255,7 +255,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/debug/mutexes.h) \
     $(wildcard include/config/have/arch/mutex/cpu/relax.h) \
   include/linux/wait.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/current.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/current.h \
   include/linux/klist.h \
   include/linux/pinctrl/devinfo.h \
   include/linux/pm.h \
@@ -265,7 +265,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/pm/clk.h) \
     $(wildcard include/config/pm/generic/domains.h) \
   include/linux/completion.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/device.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/device.h \
     $(wildcard include/config/dmabounce.h) \
     $(wildcard include/config/iommu/api.h) \
     $(wildcard include/config/arm/dma/use/iommu.h) \
@@ -278,7 +278,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
   include/linux/bitmap.h \
   include/linux/string.h \
     $(wildcard include/config/binary/printf.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/string.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/string.h \
   include/linux/sched.h \
     $(wildcard include/config/runtime/compcache.h) \
     $(wildcard include/config/sched/debug.h) \
@@ -356,7 +356,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/page/poisoning.h) \
     $(wildcard include/config/page/guard.h) \
     $(wildcard include/config/page/debug/something/else.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/page.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/page.h \
     $(wildcard include/config/cpu/copy/v3.h) \
     $(wildcard include/config/cpu/copy/v4wt.h) \
     $(wildcard include/config/cpu/copy/v4wb.h) \
@@ -366,9 +366,9 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/cpu/copy/v6.h) \
     $(wildcard include/config/kuser/helpers.h) \
     $(wildcard include/config/have/arch/pfn/valid.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/glue.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/pgtable-2level-types.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/memory.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/glue.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/pgtable-2level-types.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/memory.h \
     $(wildcard include/config/need/mach/memory/h.h) \
     $(wildcard include/config/page/offset.h) \
     $(wildcard include/config/dram/size.h) \
@@ -395,18 +395,18 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/discontigmem.h) \
     $(wildcard include/config/sparsemem/vmemmap.h) \
   include/asm-generic/getorder.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/mmu.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/mmu.h \
     $(wildcard include/config/cpu/has/asid.h) \
   arch/arm/include/generated/asm/cputime.h \
   include/asm-generic/cputime.h \
   include/linux/smp.h \
     $(wildcard include/config/use/generic/smp/helpers.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/smp.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/smp.h \
   include/linux/sem.h \
   include/linux/ipc.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/ipcbuf.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/ipcbuf.h \
   include/asm-generic/ipcbuf.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/sembuf.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/sembuf.h \
   include/linux/rcupdate.h \
     $(wildcard include/config/rcu/torture/test.h) \
     $(wildcard include/config/tree/rcu.h) \
@@ -417,9 +417,9 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/preempt/rt.h) \
   include/linux/rcutree.h \
   include/linux/signal.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/signal.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/signal.h \
   include/asm-generic/signal-defs.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/sigcontext.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/sigcontext.h \
   arch/arm/include/generated/asm/siginfo.h \
   include/asm-generic/siginfo.h \
   include/linux/pid.h \
@@ -465,7 +465,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/have/arch/nodedata/extension.h) \
   include/linux/notifier.h \
   include/linux/srcu.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/topology.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/topology.h \
     $(wildcard include/config/arm/cpu/topology.h) \
   include/asm-generic/topology.h \
   include/linux/proportions.h \
@@ -510,7 +510,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/constructors.h) \
     $(wildcard include/config/debug/set/module/ronx.h) \
   include/linux/stat.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/stat.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/stat.h \
   include/linux/kmod.h \
   include/linux/gfp.h \
   include/linux/mmdebug.h \
@@ -518,8 +518,8 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/debug/virtual.h) \
   include/linux/elf.h \
   include/linux/elf-em.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/elf.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/user.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/elf.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/user.h \
   include/linux/moduleparam.h \
     $(wildcard include/config/alpha.h) \
     $(wildcard include/config/ia64.h) \
@@ -531,7 +531,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
   include/linux/export.h \
     $(wildcard include/config/symbol/prefix.h) \
     $(wildcard include/config/modversions.h) \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/module.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/module.h \
     $(wildcard include/config/arm/unwind.h) \
   include/linux/slab.h \
     $(wildcard include/config/slab/debug.h) \
@@ -581,7 +581,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
   include/linux/dqblk_qtree.h \
   include/linux/nfs_fs_i.h \
   include/linux/fcntl.h \
-  /home/slim80/Scrivania/kernel/Note3/Fulgor_Kernel_v1.5/arch/arm/include/asm/fcntl.h \
+  /home/audax/Audax_Kernel/arch/arm/include/asm/fcntl.h \
   include/asm-generic/fcntl.h \
     $(wildcard include/config/scfs/lower/pagecache/invalidation.h) \
   include/linux/err.h \
@@ -624,6 +624,7 @@ deps_arch/arm/hotplug/intelli_plug.o := \
     $(wildcard include/config/cpu/freq/default/gov/smartmax.h) \
     $(wildcard include/config/cpu/freq/default/gov/yankactive.h) \
     $(wildcard include/config/cpu/freq/default/gov/zzmoove.h) \
+  include/linux/powersuspend.h \
 
 arch/arm/hotplug/intelli_plug.o: $(deps_arch/arm/hotplug/intelli_plug.o)
 
